504cc3bfabf19649f1e6397a151147c9f8d4ec05
mariachernyshova <mary.chernishova@mail.ru>
2024-04-21 23:06:30 +0300

    use redux and review navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Catalog } from "./components/Catalog/Catalog";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
-import { Navigation } from "./components/Navigation/Navigation";
+import { Provider } from "react-redux";
+import { Catalog } from "./components/Catalog/Catalog.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import { Header } from "./components/Header/Header.jsx";
+import { Navigation } from "./components/Navigation/Navigation.jsx";
+import { store } from "./store/index.js";
 
 export const App = () => {
 
   return (
-   <>
-    <Header/>
-    <main>
-      <Navigation/>
-      <Catalog/>
-    </main>
-    <Footer/>
-   </>
+    <Provider store={store}>
+      <Header />
+      <main>
+        <Navigation />
+        <Catalog />
+      </main>
+      <Footer />
+    </Provider>
   );
 }
